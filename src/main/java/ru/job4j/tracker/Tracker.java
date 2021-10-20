dd1c6d957c944330b8689af64902225579649258
Danil <totem7h@gmail.com>
2021-10-20 12:30:34 +0400

    5. Tracker - хранилище [#396 #218157]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +24,34 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (items[index] != null && item.getName().equals(key)) {
+                result[index] = item;
+                size++;
+            }
+        }
+        result = Arrays.copyOf(result, size);
+        return result;
+    }
+
+    public Item[] findAll() {
+        Item[] itemNotNull = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item != null) {
+                itemNotNull[i] = item;
+                size++;
+            }
+        }
+        itemNotNull = Arrays.copyOf(itemNotNull, size);
+        for (int i = 0; i < itemNotNull.length; i++) {
+            System.out.println(itemNotNull[i]);
+        }
+        return itemNotNull;
     }
 }
