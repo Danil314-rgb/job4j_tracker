41a4de244674fe91000309f001def8e54401e1e0
Danil <totem7h@gmail.com>
2021-10-21 12:37:18 +0400

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/tracker/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int passengers) {
+
+    }
+
+    @Override
+    public double refuel(double count) {
+        return 0;
+    }
+}
