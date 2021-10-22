dfffbd1201428542368e370b7e86cfff1d8dddeb
Danil <totem7h@gmail.com>
2021-10-22 17:34:26 +0400

    9. Написать тесты на StartUI. [#785]

--- src/main/java/ru/job4j/strategy/Triangle.java
+++ src/main/java/ru/job4j/strategy/Triangle.java
@@ -6,9 +6,7 @@ public class Triangle implements Shape {
     public String draw() {
 
         String ln = System.lineSeparator();
-        return  "   ^  " + ln +
-                " /   \\" + ln +
-                "/_____\\" + ln;
+        return "   ^  " + ln + " /   \\" + ln + "/_____\\" + ln;
 
     }
 }
