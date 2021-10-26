59a4eefca72666e95cbd46ca0cce5d3231923b82
Danil <totem7h@gmail.com>
2021-10-26 12:02:31 +0400

    9.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 
 public class StartUITest {
 
-    @Test
+    /*@Test
     public void whenExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
@@ -20,6 +20,30 @@ public class StartUITest {
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
                 "Menu:" + System.lineSeparator() + "0. Exit Program" + System.lineSeparator()
+        ));
+    }*/
+
+    @Test
+    public void whenReplace() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        String replaceName = "New Test Name";
+        Input in = new StubInput(
+                new String[] {"0", String.valueOf(one.getId()), replaceName, "1"}
+        );
+        UserAction[] actions = new UserAction[] {
+                new EditAction(out), new ExitProgramAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is("Menu:"
+                + ln
+                + "0. Edit item" + ln
+                + "1. Exit Program" + ln
+                + "Menu:" + ln
+                + "0. Edit item" + ln
+                + "1. Exit Program" + ln
         ));
     }
 
