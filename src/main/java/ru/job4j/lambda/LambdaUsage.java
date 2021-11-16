f81ecd567d482b2eee3700d3821e5cbc22254708
Danil <totem7h@gmail.com>
2021-11-16 11:48:23 +0400

    2.2. Лямбда блок [#249208 #228872]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -18,7 +18,7 @@ public class LambdaUsage {
 
         Comparator<String> cmpDescSize = (left, right) -> {
             System.out.println("compare - " + left.length() + " : " + right.length());
-            return left.length() - right.length();
+            return Integer.compare(left.length(), right.length());
         };
     }
 }
