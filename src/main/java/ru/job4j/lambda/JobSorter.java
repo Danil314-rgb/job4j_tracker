39e001f89fafa0fba457f4425f40be7361f02ef2
Danil <totem7h@gmail.com>
2021-11-17 12:41:50 +0400

    -

---
+++ src/main/java/ru/job4j/lambda/JobSorter.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JobSorter {
+
+    public static void main(String[] args) {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+    }
+}
