97cc2c5cb77b753aad4f8f67184dc8ca99bb4db8
Danil <totem7h@gmail.com>
2021-11-18 13:57:27 +0400

    2. Список адресов [#110062 #229552]

--- src/main/java/ru/job4j/stremapi/Profile.java
+++ src/main/java/ru/job4j/stremapi/Profile.java
@@ -2,6 +2,10 @@ package ru.job4j.stremapi;
 
 public class Profile {
 
+    public Profile(Address address) {
+        this.address = address;
+    }
+
     private Address address;
 
     public Address getAddress() {
--- src/test/java/ru/job4j/stremapi/ProfilesTest.java
+++ src/test/java/ru/job4j/stremapi/ProfilesTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ProfilesTest {
@@ -13,9 +14,14 @@ public class ProfilesTest {
     public void whenAddress() {
         Address address1 = new Address("Moscow", "Red", 15, 55);
         Address address2 = new Address("Moscow", "Green", 56, 78);
+        Profile profile1 = new Profile(address1);
+        Profile profile2 = new Profile(address2);
+        List<Profile> listProf = List.of(profile1, profile2);
 
-        List<Profile> profiles = List.of(new Profile());
-
+        Profiles profiles = new Profiles();
+        List<Address> res = profiles.collect(listProf);
+        List<Address> expected = List.of(address1, address2);
+        assertThat(res, is(expected));
     }
 
 }
